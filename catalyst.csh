c693ff0969049861e4e3b1481988fe7a0c478439
Stewart Adam <s.adam@diffingo.com>
2011-06-18 20:24:21 +0000

    * Sat Jun 18 2011 Stewart Adam <s.adam at diffingo.com> 11.6-1 * Update to Catalyst 11.6 (internal version 8.86.1)

--- catalyst.csh
+++ catalyst.csh
@@ -1,9 +1,9 @@
 set arch = `uname -m`
 
 if ($arch == x86_64) then
-    setenv LIBGL_DRIVERS_PATH /usr/lib64/dri:/usr/lib/dri
-    #setenv LD_LIBRARY_PATH /usr/lib64/fglrx:/usr/lib/fglrx:$LD_LIBRARY_PATH
+    setenv LIBGL_DRIVERS_PATH /usr/lib64/dri:/usr/lib/dri:$LIBGL_DRIVERS_PATH
+    setenv LD_LIBRARY_PATH /usr/lib64/catalyst:/usr/lib/catalyst:$LD_LIBRARY_PATH
 else if ($arch =~ i[3-6]86) then
-    setenv LIBGL_DRIVERS_PATH /usr/lib/dri
-    #setenv LD_LIBRARY_PATH /usr/lib/fglrx:$LD_LIBRARY_PATH
+    setenv LIBGL_DRIVERS_PATH /usr/lib/dri:$LIBGL_DRIVERS_PATH
+    setenv LD_LIBRARY_PATH /usr/lib/catalyst:$LD_LIBRARY_PATH
 endif
--- catalyst.sh
+++ catalyst.sh
@@ -1,8 +1,9 @@
 ARCH="$(uname -m)"
 
 case "$ARCH" in
-        x86_64) export LIBGL_DRIVERS_PATH="/usr/lib64/dri:/usr/lib/dri";;
-                #export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/lib64/fglrx:/usr/lib/fglrx";;
-    i[3-6\d]86) export LIBGL_DRIVERS_PATH="/usr/lib/dri";;
-                #export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/usr/lib/fglrx";;
+        x86_64) export LIBGL_DRIVERS_PATH="/usr/lib64/dri:/usr/lib/dri:$LIBGL_DRIVERS_PATH";
+                export LD_LIBRARY_PATH="/usr/lib64/fglrx:/usr/lib/catalyst:$LD_LIBRARY_PATH";;
+    i[3-6\d]86) export LIBGL_DRIVERS_PATH="/usr/lib/dri:$LIBGL_DRIVERS_PATH";
+                export LD_LIBRARY_PATH="/usr/lib/catalyst:$LD_LIBRARY_PATH";;
 esac
+
